c2c4c72425f86b1c6e16b4065a7612f163014933
shreyas bole <shreyasbole4@gmail.com>
2024-04-28 22:45:18 +0530

    Add initial server setup and configuration

---
+++ src/app/index.ts
@@ -0,0 +1,18 @@
+import Express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+
+export async function initServer() {
+  const app = Express();
+
+  const graphqlServer = new ApolloServer({
+    typeDefs: "",
+    resolvers: {},
+  });
+
+  await graphqlServer.start();
+
+  app.use("/graphql", expressMiddleware(graphqlServer));
+
+  return app;
+}
