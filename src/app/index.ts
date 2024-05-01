0226376f2c9fc379f6f956d3c9b9e4b2a830471a
shreyas bole <shreyasbole4@gmail.com>
2024-05-01 23:04:21 +0530

    Update server configuration and add body-parser dependency

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,18 +1,30 @@
-import Express from "express";
+import express from "express";
+import bodyParser from "body-parser";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 
 export async function initServer() {
-  const app = Express();
+  const app = express();
+
+  app.use(bodyParser.json());
 
   const graphqlServer = new ApolloServer({
-    typeDefs: "",
-    resolvers: {},
+    typeDefs: `
+    type Query {
+      sayHello: String 
+    }
+    
+    `,
+    resolvers: {
+      Query: {
+        sayHello: () => "Hello, World!",
+      },
+    },
   });
 
   await graphqlServer.start();
 
-  // app.use("/graphql", expressMiddleware(graphqlServer));
+  app.use("/graphql", expressMiddleware(graphqlServer));
 
   return app;
 }
