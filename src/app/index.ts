06227f3052d321fbb59efa399f2ec6ec5d6e93c7
shreyas bole <shreyasbole4@gmail.com>
2024-05-05 22:33:59 +0530

    refactor: Remove unused code and dependencies in server configuration

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,34 +1,34 @@
-import express from "express";
-import bodyParser from "body-parser";
-import { ApolloServer } from "@apollo/server";
-import { expressMiddleware } from "@apollo/server/express4";
-import { prismaClient } from "../clients/db";
+// import express from "express";
+// import bodyParser from "body-parser";
+// import { ApolloServer } from "@apollo/server";
+// import { expressMiddleware } from "@apollo/server/express4";
+// import { prismaClient } from "../clients/db";
 
-export async function initServer() {
-  const app = express();
+// export async function initServer() {
+//   const app = express();
 
-  app.use(bodyParser.json());
+//   app.use(bodyParser.json());
 
-  const graphqlServer = new ApolloServer({
-    typeDefs: `
-    type Query {
-      sayHello: String 
-      sayHelloToMe(name: String!): String
-    }
+//   const graphqlServer = new ApolloServer({
+//     typeDefs: `
+//     type Query {
+//       sayHello: String 
+//       sayHelloToMe(name: String!): String
+//     }
     
-    `,
-    resolvers: {
-      Query: {
-        sayHello: () => "Hello, World!",
-        sayHelloToMe: (parent: any, { name }: { name: string }) =>
-          `Hello, ${name}!`,
-      },
-    },
-  });
+//     `,
+//     resolvers: {
+//       Query: {
+//         sayHello: () => "Hello, World!",
+//         sayHelloToMe: (parent: any, { name }: { name: string }) =>
+//           `Hello, ${name}!`,
+//       },
+//     },
+//   });
 
-  await graphqlServer.start();
+//   await graphqlServer.start();
 
-  app.use("/graphql", expressMiddleware(graphqlServer));
+//   app.use("/graphql", expressMiddleware(graphqlServer));
 
-  return app;
-}
+//   return app;
+// }
